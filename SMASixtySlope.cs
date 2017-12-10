6634b10a56ee3dae625521da2fe0008e416af88f
renhaoran <RHRxjtu@outlook.com>
2017-12-10 11:22:37 +0800

    Extension of SMA Sixty

---
+++ SMASixtySlope.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using QuantTC;
+using QuantTC.Indicators.Generic;
+
+namespace QuantIX.SMA
+{
+    public class SMASixtySlope : Indicator<int>
+    {
+        public SMASixtySlope(IIndicator<double> source)
+        {
+            Source = source;
+            Source.Update += SourceOnUpdate;
+        }
+
+        private void SourceOnUpdate()
+        {
+            Data.FillRange(Count, Source.Count, i => i > 0 ? (int)(Source[i] + 0.5) - (int)(Source[i - 1] + 0.5) : 0);
+            FollowUp();
+        }
+
+
+        public IIndicator<double> Source { get; }
+    }
+}
